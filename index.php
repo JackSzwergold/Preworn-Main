66720ef51abde74476535b4b5a682b8734d76458
Jack Szwergold <JackSzwergold@gmail.com>
2014-01-20 23:28:49 -0500

    Making this page real.

--- index.php
+++ index.php
@@ -58,7 +58,7 @@ $wrapper = '<div class="Wrapper">'
          . '<br />'
 
          . '<h3>current projects</h3>'
-         . '<p><a href="mosaic/" title="mosaic" name="mosaic" target="_top">image mosaic</a>: a dynamically generated image mosaic using php, the gd graphics libarary, html & css</p>'
+         . '<p><a href="mosaic/" title="image mosaic" name="image mosaic" target="_top">image mosaic</a>: a dynamically generated image mosaic using php, the gd graphics libarary, html & css</p>'
          . '<br />'
 
          . '<h3>skills</h3>'
