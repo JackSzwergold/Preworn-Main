7938dae67f8f976db8cada6db12dc2d198882abb
Jack Szwergold <JackSzwergold@gmail.com>
2014-01-22 22:09:32 -0500

    Forcing a frontend display class into existence.

---
+++ common/functions.inc.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * Preworn Functions (functions.inc.php)
+ *
+ * Programming: Jack Szwergold <JackSzwergold@gmail.com>
+ *
+ * Created: 2014-01-22, js
+ * Version: 2014-01-22, js: creation
+ *          2014-01-22, js: development & cleanup
+ *
+ */
+
+// Nice PHP JSON formatting function.
+// SOURCE: http://stackoverflow.com/questions/6054033/pretty-printing-json-with-php/9776726#9776726
+function prettyPrint ($json) {
+
+  $result = '';
+  $level = 0;
+  $prev_char = '';
+  $in_quotes = false;
+  $ends_line_level = NULL;
+  $json_length = strlen($json);
+
+  for ($i = 0; $i < $json_length; $i++) {
+    $char = $json[$i];
+    $new_line_level = NULL;
+    $post = "";
+    if ($ends_line_level !== NULL) {
+      $new_line_level = $ends_line_level;
+      $ends_line_level = NULL;
+    }
+    if ($char === '"' && $prev_char != '\\') {
+      $in_quotes = !$in_quotes;
+    }
+    elseif (!$in_quotes) {
+      switch( $char ) {
+        case '}': case ']':
+          $level--;
+          $ends_line_level = NULL;
+          $new_line_level = $level;
+          break;
+        case '{': case '[':
+          $level++;
+        case ',':
+          $ends_line_level = $level;
+          break;
+        case ':':
+          $post = " ";
+          break;
+        case " ": case "\t": case "\n": case "\r":
+          $char = "";
+          $ends_line_level = $new_line_level;
+          $new_line_level = NULL;
+          break;
+        }
+      }
+      if ($new_line_level !== NULL) {
+        $result .= "\n".str_repeat( "\t", $new_line_level );
+      }
+      $result .= $char.$post;
+      $prev_char = $char;
+  }
+  return $result;
+
+} // prettyPrint
+
+?>
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Preworn Index (index.php)
+ * Preworn Index Controller (index.php)
  *
  * Programming: Jack Szwergold <JackSzwergold@gmail.com>
  *
@@ -12,158 +12,36 @@
  */
 
 //**************************************************************************************//
-// Redirect & debugging stuff.
+// Require the basic configuration settings & functions.
 
-# phpinfo();
-# header('Location: /mosaic/');
-# echo $_SERVER['SERVER_NAME'];
-# echo '<br />';
-# echo $_SERVER['SERVER_ADDR'];
-# die();
-
-//**************************************************************************************//
-// Require Parsedown
-
+require_once 'common/functions.inc.php';
+require_once 'lib/frontendDisplay.class.php';
 require_once 'lib/Parsedown.php';
 
-//**************************************************************************************//
-// Set the mode.
-
-$mode = 'mega';
 
 //**************************************************************************************//
-// Set the sundry metadata stuff.
+// Define the valid arrays.
 
-$description = 'this site is jack szwergold’s the calling card, gallery, portfolio, playground, white wall, black box, idea sandbox &amp; daily distraction.';
-
-//**************************************************************************************//
-// Load the markdown verskon of the body text.
-
-$md_body = file_get_contents('index.md');
-$body = Parsedown::instance()->parse($md_body);
+$VALID_CONTROLLERS = array('portfolio');
+$DISPLAY_CONTROLLERS = array('portfolio');
+$VALID_GET_PARAMETERS = array('_debug', 'portfolio');
+$VALID_CONTENT_TYPES = array('application/json','text/plain','text/html');
+$VALID_CHARSETS = array('utf-8','iso-8859-1','cp-1252');
 
 //**************************************************************************************//
-// Set the content in the wrapper area.
+// Set config options.
 
-$wrapper = '<div class="Wrapper">'
-         . '<div class="Padding">'
-
-         . '<div class="Content">'
-         . '<div class="Padding">'
-
-         . '<div class="Section">'
-         . '<div class="Padding">'
-         . '<div class="Middle">'
-
-         . '<div class="Core">'
-         . '<div class="Padding">'
-
-         . '<div class="Grid">'
-         . '<div class="Padding">'
-         
-         . '<div class="PixelBoxWrapper">'
-         
-         . $body
-
-         . '</div><!-- .PixelBoxWrapper -->'
-
-         . '</div><!-- .Padding -->'
-         . '</div><!-- .Grid -->'
-
-         . '</div><!-- .Middle -->'
-         . '</div><!-- .Padding -->'
-         . '</div><!-- .Section -->'
-
-         . '</div><!-- .Padding -->'
-         . '</div><!-- .Core -->'
-
-         . '</div><!-- .Padding -->'
-         . '</div><!-- .Content -->'
-
-         . '</div><!-- .Padding -->'
-         . '</div><!-- .Wrapper -->'
-         ;
+$DEBUG_OUTPUT_JSON = false;
 
 //**************************************************************************************//
-// Set the view wrapper.
+// Init the "frontendDisplay()" class.
 
-$body = sprintf('<div class="%sView">', $mode)
-      . $wrapper
-      . sprintf('</div><!-- .%sView -->', $mode)
-      ;
+$frontendDisplayClass = new frontendDisplay('text/html', 'utf-8', FALSE, FALSE);
+$frontendDisplayClass->setViewMode('mega');
+$frontendDisplayClass->setPageDescription('this site is jack szwergold’s the calling card, gallery, portfolio, playground, white wall, black box, idea sandbox &amp; daily distraction.');
+$content = $frontendDisplayClass->setBody('index.md');
+$frontendDisplayClass->initContent($content);
 
-//**************************************************************************************//
-// Set the favicons.
 
-$favicons = array();
-
-$favicons[] = '<!-- Opera Speed Dial Favicon -->'
-            . '<link rel="icon" type="image/png" href="favicons/speeddial-160px.png" />'
-            ;
-
-$favicons[] = '<!-- Standard Favicon -->'
-            . '<link rel="icon" type="image/x-icon" href="favicons/favicon.ico" />'
-            ;
-
-$favicons[] = '<!-- For iPhone 4 Retina display: -->'
-            . '<link rel="apple-touch-icon-precomposed" sizes="114x114" href="favicons/apple-touch-icon-114x114-precomposed.png" />'
-            ;
-
-$favicons[] = '<!-- For iPad: -->'
-            . '<link rel="apple-touch-icon-precomposed" sizes="72x72" href="favicons/apple-touch-icon-72x72-precomposed.png" />'
-            ;
-
-$favicons[] = '<!-- For iPhone: -->'
-            . '<link rel="apple-touch-icon-precomposed" href="favicons/apple-touch-icon-57x57-precomposed.png" />'
-            ;
-
-//**************************************************************************************//
-// Doctype.
-
-if (FALSE) {
-  $doctype = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">';
-  $html = '<html xmlns="http://www.w3.org/1999/xhtml">';
-  $meta_copyright = '<meta name="copyright" content="(c) copyright ' . date('Y') . ' jack szwergold. all rights reserved." />';
-}
-else {
-  $doctype = '<!DOCTYPE html>';
-  $html = '<html lang="en">';
-  $meta_copyright = '<meta name="dcterms.rightsHolder" content="(c) copyright ' . date('Y') . ' jack szwergold. all rights reserved.">';
-}
-
-//**************************************************************************************//
-// Return the output.
-
-echo $doctype
-   . $html
-   . '<head>'
-   . '<title>preworn</title>'
-   . '<meta http-equiv="content-type" content="text/html; charset=utf-8" />'
-   . '<meta name="description" content="' . $description . '" />'
-   . $meta_copyright
-   . '<meta property="og:title" content="image mosaic" />'
-   . '<meta property="og:description" content="' . $description . '" />'
-   . '<meta property="og:type" content="website" />'
-   . '<meta property="og:locale" content="en_US" />'
-   . '<meta property="og:url" content="http://www.preworn.com/" />'
-   . '<meta property="og:site_name" content="preworn" />'
-   . '<meta property="og:image" content="http://www.preworn.com/favicons/speeddial-160px.png" />'
-   . '<meta name="robots" content="noindex,nofollow" />'
-   . '<meta name="viewport" content="width=device-width, initial-scale=0.4, maximum-scale=2, minimum-scale=0.4, user-scalable=yes" />'
-   . '<link rel="stylesheet" href="css/style.css" type="text/css" />'
-
-   . join('', $favicons)
-
-   . '<script src="script/json2.js" type="text/javascript"></script>'
-   . '<script type="text/javascript" src="script/jquery/jquery-1.10.2.min.js"></script>'
-   . '<script type="text/javascript" src="script/jquery/jquery.noconflict.js"></script>'
-   # . '<script type="text/javascript" src="script/common.js"></script>'
-
-   . '</head>'
-   . '<body>'
-   . $body
-   . '</body>'
-   . '</html>'
-   ;
 
 ?>
