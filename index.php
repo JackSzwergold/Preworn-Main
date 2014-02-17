4e8c4b3bd004d0363d364d2c54d76228d4db52db
Jack Szwergold <JackSzwergold@gmail.com>
2014-02-16 23:36:04 -0500

    More refinements.

--- index.php
+++ index.php
@@ -27,31 +27,43 @@ require_once BASE_FILEPATH . '/lib/Parsedown.php';
 $DEBUG_OUTPUT_JSON = false;
 
 //**************************************************************************************//
-// Get the URL param & set the markdown file.
+// Get the URL param & set the markdown file as well as the page title.
 
+// Init the arrays.
 $markdown_parts = array();
+$title_parts = array('preworn');
 
+// Check if a 'controller' exists.
 if (array_key_exists('controller', $_GET) && !empty($_GET['controller'])) {
   if (in_array('controller', $VALID_CONTROLLERS)) {
     $markdown_parts[] = $_GET['controller'];
+    $title_parts[] = $_GET['controller'];
+
+    // Check if a 'page' exists.
+    if (array_key_exists('page', $_GET) && !empty($_GET['page'])) {
+      $markdown_parts[] = $_GET['page'];
+      $title_parts[] = $_GET['page'];
+    }
+
   }
 }
-else {
-    $markdown_parts[] = 'index';
-}
 
-if (array_key_exists('page', $_GET) && !empty($_GET['page'])) {
-  $markdown_parts[] = $_GET['page'];
-}
-
+// Set the final markdown file path.
 $markdown_file = 'markdown/' . join('/', $markdown_parts) . '.md';
+
+if (!file_exists($markdown_file)) {
+  $markdown_file = 'markdown/index.md';
+}
+
+$page_title = join(' / ', $title_parts);
+$page_title = preg_replace('/_/', ' ', $page_title);
 
 //**************************************************************************************//
 // Init the "frontendDisplay()" class.
 
 $frontendDisplayClass = new frontendDisplay('text/html', 'utf-8', FALSE, FALSE);
 $frontendDisplayClass->setViewMode('mega');
-$frontendDisplayClass->setPageTitle('preworn');
+$frontendDisplayClass->setPageTitle($page_title);
 $frontendDisplayClass->setPageDescription('this site is jack szwergold’s the calling card, gallery, portfolio, playground, white wall, black box, idea sandbox &amp; daily distraction.');
 $frontendDisplayClass->setPageContentMarkdown($markdown_file);
 // $frontendDisplayClass->setPageContent('Hello world!');
