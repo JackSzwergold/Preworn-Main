5c9ef45ac52620b413a791725086fc41a277ae07
Jack Szwergold <JackSzwergold@gmail.com>
2015-11-10 22:05:22 -0500

    Refining the paramter parsing.

--- lib/frontendDisplayHelpers.php
+++ lib/frontendDisplayHelpers.php
@@ -19,55 +19,66 @@
  */
 
 //**************************************************************************************//
-// Require the basic configuration settings & functions.
+// Require the basics.
 
 require_once BASE_FILEPATH . '/lib/Parsedown.php';
 
 //**************************************************************************************//
-// Get the URL param & set the markdown file as well as the page title.
+// Here is the function to parse the parameters.
 
-// Init the arrays.
-$url_parts = array();
-$markdown_parts = array();
-$title_parts = array($SITE_TITLE);
+function parse_parameters ($SITE_TITLE, $VALID_GET_PARAMETERS) {
 
-// Parse the '$_GET' parameters.
-foreach($VALID_GET_PARAMETERS as $get_parameter) {
-  $$get_parameter = '';
-  if (array_key_exists($get_parameter, $_GET) && !empty($_GET[$get_parameter])) {
-    if (in_array($get_parameter, $VALID_GET_PARAMETERS)) {
-      $$get_parameter = $_GET[$get_parameter];
+  // Init the arrays.
+  $url_parts = array();
+  $markdown_parts = array();
+  $title_parts = array($SITE_TITLE);
+
+  // Parse the '$_GET' parameters.
+  foreach($VALID_GET_PARAMETERS as $get_parameter) {
+    $$get_parameter = '';
+    if (array_key_exists($get_parameter, $_GET) && !empty($_GET[$get_parameter])) {
+      if (in_array($get_parameter, $VALID_GET_PARAMETERS)) {
+        $$get_parameter = $_GET[$get_parameter];
+      }
     }
   }
-}
 
-// Set the controller.
-if (!empty($controller)) {
-  $url_parts[] = $controller;
-  $markdown_parts[] = $controller;
-  $title_parts[] = ucwords($controller);
-  if (empty($page)) {
-    $markdown_parts[] = 'index';
+  // Set the controller.
+  if (!empty($controller)) {
+    $url_parts[] = $controller;
+    $markdown_parts[] = $controller;
+    $title_parts[] = ucwords($controller);
+    if (empty($page)) {
+      $markdown_parts[] = 'index';
+    }
   }
+
+  // Set the page.
+  if (!empty($controller) && !empty($page)) {
+    $url_parts[] = $page;
+    $markdown_parts[] = $page;
+    $title_parts[] = $page;
+  }
+
+  // Set the final markdown file path.
+  $markdown_file = 'markdown/' . join('/', $markdown_parts) . '.md';
+
+  if (!file_exists($markdown_file)) {
+    $markdown_file = 'markdown/index.md';
+    $title_parts = array($SITE_TITLE);
+  }
+
+  // Set the page title.
+  $page_title = join(' / ', $title_parts);
+  $page_title = ucwords(preg_replace('/_/', ' ', $page_title));
+
+  return array($controller, $page, $page_title, $url_parts, $markdown_parts, $markdown_file);
+
 }
 
-// Set the page.
-if (!empty($controller) && !empty($page)) {
-  $url_parts[] = $page;
-  $markdown_parts[] = $page;
-  $title_parts[] = $page;
-}
+//**************************************************************************************//
+// Run the actual function and get the parts.
 
-// Set the final markdown file path.
-$markdown_file = 'markdown/' . join('/', $markdown_parts) . '.md';
-
-if (!file_exists($markdown_file)) {
-  $markdown_file = 'markdown/index.md';
-  $title_parts = array($SITE_TITLE);
-}
-
-// Set the page title.
-$page_title = join(' / ', $title_parts);
-$page_title = ucwords(preg_replace('/_/', ' ', $page_title));
+list($controller, $page, $page_title, $url_parts, $markdown_parts, $markdown_file) = parse_parameters($SITE_TITLE, $VALID_GET_PARAMETERS);
 
 ?>
