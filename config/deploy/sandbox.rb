1518b8a8662e5c76651443736159b3db31914cab
Jack Szwergold <JackSzwergold@gmail.com>
2016-04-20 19:49:45 -0400

    Consolidating some stuff into the main 'deploy.rb' file.

--- config/deploy/sandbox.rb
+++ config/deploy/sandbox.rb
@@ -25,46 +25,4 @@ set :deploy_to, "/var/www/builds/#{fetch(:application)}/#{fetch(:deployment_type
 # Disable warnings about the absence of the styleseheets, javscripts & images directories.
 set :normalize_asset_timestamps, false
 
-# Create the 'create_symlink' task to create symbolic links and other related items.
-namespace :deploy do
-
-  desc "Set the symbolic links."
-  task :create_symlink do
-    on roles(:app) do
-
-      # info "Link the cheat sheet stuff to 'tutorials_and_cheat_sheets'."
-      execute "cd #{current_path} && ln -s #{fetch(:web_builds)}/tutorials_and_cheat_sheets/#{fetch(:deployment_type)}/current markdown/tutorials_and_cheat_sheets"
-
-      # info "Link the image mosaic stuff to 'mosaic'."
-      execute "cd #{current_path} && ln -sf #{fetch(:web_builds)}/mosaic/#{fetch(:deployment_type)}/current #{fetch(:projects_path)}/mosaic"
-
-      # info "Link the mosaic javascript stuff to 'mosaic_js'."
-      execute "cd #{current_path} && ln -sf #{fetch(:web_builds)}/mosaic_js_dist/#{fetch(:deployment_type)}/current #{fetch(:projects_path)}/mosaic_js"
-
-      # info "Link the preworn ascii art stuff to 'ascii'."
-      execute "cd #{current_path} && ln -sf #{fetch(:web_builds)}/ascii/#{fetch(:deployment_type)}/current #{fetch(:projects_path)}/ascii"
-
-      # info "Link the preworn slider stuff to 'slider'."
-      execute "cd #{current_path} && ln -sf #{fetch(:web_builds)}/slider/#{fetch(:deployment_type)}/current #{fetch(:projects_path)}/slider"
-
-      # info "Link the colorspace conversions stuff to 'colorspace'."
-      execute "cd #{current_path} && ln -sf #{fetch(:web_builds)}/colorspace_conversions/#{fetch(:deployment_type)}/current projects/colorspace"
-
-      # info "If there is no directory & no symbolic link to '#{fetch(:short_name)}' then create a directory named '#{fetch(:short_name)}'."
-      # execute "cd #{fetch(:live_root)} && if [ ! -d #{fetch(:short_name)} ]; then if [ ! -h #{fetch(:short_name)} ]; then mkdir ./#{fetch(:short_name)}; fi; fi"
-
-      # info "If there is no symbolic link called #{fetch(:short_name)}' and '#{fetch(:short_name)}' is a directory, delete that directory."
-      execute "cd #{fetch(:live_root)} && if [ ! -h #{fetch(:short_name)} ]; then if [ -d #{fetch(:short_name)} ]; then rm -rf ./#{fetch(:short_name)}; fi; fi"
-
-      # info "If there is a symbolic link called '#{fetch(:short_name)}', delete that directory."
-      execute "cd #{fetch(:live_root)} && if [ -h #{fetch(:short_name)} ]; then rm ./#{fetch(:short_name)}; fi"
-
-      # info "If there is a symbolic link to '#{fetch(:short_name)}' then create a symbolic link called '#{fetch(:short_name)}'."
-      execute "cd #{fetch(:live_root)} && if [ ! -h #{fetch(:short_name)} ]; then if [ ! -d #{fetch(:short_name)} ]; then ln -sf #{current_path} ./#{fetch(:short_name)}; fi; fi"
-
-    end
-  end
-
-end
-
 after "deploy:published", "deploy:create_symlink"
