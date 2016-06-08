d91c98f37e7bb11e61833a2cb83c494dc6b9f676
Jack Szwergold <JackSzwergold@gmail.com>
2016-06-08 17:03:50 -0400

    Adding a header to the mix and tweaking shadows.

--- index.php
+++ index.php
@@ -93,12 +93,19 @@ $frontendDisplayClass->setPaymentInfo($PAYMENT_INFO);
 // Init header and footer stuff.
 
 $navigation = $frontendDisplayClass->setNavigation();
-$frontendDisplayClass->setBodyHeader($navigation);
+
+$header = '<div class="Header">' 
+        . $navigation
+        . '</div>'
+        ;
+$frontendDisplayClass->setBodyHeader($header);
+
 $amazon_ad_468x60 = '<iframe src="//rcm-na.amazon-adsystem.com/e/cm?o=1&p=13&l=ez&f=ifr&linkID=feede769df2856e2565b6b6685a88b80&t=preworn-20&tracking_id=preworn-20" width="468" height="60" style="border: none;"></iframe>';
 $footer = '<div class="Footer">' 
         . $amazon_ad_468x60
         . '</div>'
         ;
+
 $frontendDisplayClass->setBodyFooter($footer);
 
 //**************************************************************************************//
