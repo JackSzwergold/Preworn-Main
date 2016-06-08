3d45f367f05075bfb668f330ea6e857b25598c8e
Jack Szwergold <JackSzwergold@gmail.com>
2016-06-08 00:55:09 -0400

    Adding ads to the footer for now.

--- index.php
+++ index.php
@@ -92,7 +92,8 @@ $frontendDisplayClass->setPaymentInfo($PAYMENT_INFO);
 
 $nameplate = $frontendDisplayClass->setNameplate();
 $frontendDisplayClass->setBodyHeader($nameplate);
-// $frontendDisplayClass->setBodyFooter();
+$amazon_ad_468x60 = '<iframe src="//rcm-na.amazon-adsystem.com/e/cm?o=1&p=13&l=ez&f=ifr&linkID=feede769df2856e2565b6b6685a88b80&t=preworn-20&tracking_id=preworn-20" width="468" height="60" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe>';
+$frontendDisplayClass->setBodyFooter($amazon_ad_468x60 . '<br /><br />');
 
 //**************************************************************************************//
 // Init the content.
