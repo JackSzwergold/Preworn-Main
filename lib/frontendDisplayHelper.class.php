33d952b767e3fc1f4399de5b277fc3c2ab80d380
Jack Szwergold <JackSzwergold@gmail.com>
2016-07-26 23:30:16 -0400

    Shuffling and reformatting of the 'frontendDisplayHelper' stuff.

--- lib/frontendDisplayHelper.class.php
+++ lib/frontendDisplayHelper.class.php
@@ -15,8 +15,10 @@
  * Created: 2015-11-10, js
  * Version: 2015-11-10, js: creation
  *          2015-11-10, js: development
+ *          2016-07-26, js: refactoring and cleanup
  *
  */
+
 
 //**************************************************************************************//
 // The beginnings of a front end display helper class.
@@ -31,56 +33,116 @@ class frontendDisplayHelper {
   private $count = 1;
 
   private $url_parts = array();
-  private $VIEW_MODE = '';
+  private $VIEW_MODE = null;
   private $DEBUG_MODE = FALSE;
   private $html_content = '';
   private $json_content = '';
 
+
   //**************************************************************************************//
   // Set the default controller.
   public function setDefaultController ($value) {
+
     if (!empty($value)) {
       $this->controller_default = $value;
     }
+
   } // setDefaultController
 
 
- //**************************************************************************************//
+  //**************************************************************************************//
   // Set the selected controller.
   public function setSelectedController ($value) {
+
     if (!empty($value)) {
       $this->controller_select = $value;
     }
+
   } // setSelectedController
 
 
   //**************************************************************************************//
   // Set the page base.
   public function setPageBase ($value) {
+
     if (!empty($value)) {
       $this->page_base = $value;
     }
+
   } // setPageBase
 
 
   //**************************************************************************************//
   // Set the page base.
   public function setPageBaseSuffix ($value) {
+
     if (!empty($value)) {
       $this->page_base_suffix = $value;
     }
+
   } // setPageBaseSuffix
 
 
   //**************************************************************************************//
   // Set the count.
   public function setCount ($value) {
+
     if (!empty($value)) {
       $this->count = $value;
     }
+
   } // setCount
 
 
+  //**************************************************************************************//
+  //**************************************************************************************//
+  //**************************************************************************************//
+  // Get the view mode.
+  public function getViewMode () {
+
+    return $this->VIEW_MODE;
+
+  } // getViewMode
+
+
+  //**************************************************************************************//
+  // Get the page title.
+  public function getPageTitle () {
+
+    return $this->page_title;
+
+  } // getPageTitle
+
+
+  //**************************************************************************************//
+  // Get the URL parts.
+  public function getURLParts () {
+
+    return $this->url_parts;
+
+  } // getURLParts
+
+
+  //**************************************************************************************//
+  // Get the HTML content.
+  public function getHTMLContent () {
+
+    return $this->html_content;
+
+  } // getHTMLContent
+
+
+  //**************************************************************************************//
+  // Get the JSON content.
+  public function getJSONContent () {
+
+    return $this->json_content;
+
+  } // getJSONContent
+
+
+  //**************************************************************************************//
+  //**************************************************************************************//
   //**************************************************************************************//
   // Filter the view mode.
   public function filterViewMode ($mode = null, $mode_options) {
@@ -97,48 +159,13 @@ class frontendDisplayHelper {
     return $mode;
 
   } // filterViewMode
-  
-  
+
+
   public function initContent ($DEBUG_MODE = FALSE) {
 
   } // initContent
 
 
-  //**************************************************************************************//
-  // Get the view mode.
-  public function getViewMode () {
-    return $this->VIEW_MODE;
-  } // getViewMode
-
-
-  //**************************************************************************************//
-  // Get the page title.
-  public function getPageTitle () {
-    return $this->page_title;
-  } // getPageTitle
-
-
-  //**************************************************************************************//
-  // Get the URL parts.
-  public function getURLParts () {
-    return $this->url_parts;
-  } // getURLParts
-
-
-  //**************************************************************************************//
-  // Get the HTML content.
-  public function getHTMLContent () {
-    return $this->html_content;
-  } // getHTMLContent
-
-
-  //**************************************************************************************//
-  // Get the JSON content.
-  public function getJSONContent () {
-    return $this->json_content;
-  } // getJSONContent
-
-
-} // frontendDisplayHelper
+ } // frontendDisplayHelper
 
 ?>
