c8daa00a7ca727f395e02c621bc39042b2252ce5
Jack Szwergold <JackSzwergold@gmail.com>
2017-06-10 23:39:21 -0400

    Adjustments to get Capistrano 3.8.1 in place.

--- Capfile
+++ Capfile
@@ -3,3 +3,7 @@ require 'capistrano/setup'
 
 # Include default deployment tasks
 require 'capistrano/deploy'
+
+# Include default SCM as Git
+require "capistrano/scm/git"
+install_plugin Capistrano::SCM::Git
--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :short_name, 'site'
 set :repo_url, 'git@github.com:JackSzwergold/Preworn-Main.git'
 
 # Default value for :scm is :git
-set :scm, :git
+# set :scm, :git
 
 # Default value for :format is :pretty
 set :format, :pretty
