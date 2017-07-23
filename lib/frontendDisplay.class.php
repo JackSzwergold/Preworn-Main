a3842a1c77126298ed2fe210339737a8279b20f5
Jack Szwergold <JackSzwergold@gmail.com>
2017-07-23 02:05:48 -0400

    More CSS futzing.

--- lib/frontendDisplay.class.php
+++ lib/frontendDisplay.class.php
@@ -771,7 +771,9 @@ class frontendDisplay {
       $markdown_sliced = array_slice(array_values($this->markdown_parts), 0, -1);
       $back_url = BASE_PATH . join('/', $markdown_sliced);
       $li_items[] = '<li id="back">'
-                  . sprintf('<a href="%s" title="back"></a>', $back_url)
+                  . sprintf('<a href="%s" title="back">', $back_url)
+                  . 'back'
+                  . '</a>'
                   . '</li>'
                   ;
     }
